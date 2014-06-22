d8d1e8c7b42924db1a211cbcc57d77ca5feedcb6
DimitrisVlachos <DimitrisV22@gmail.com>
2014-06-22 18:19:35 -0400

    Create types.hpp

---
+++ types.hpp
@@ -0,0 +1,23 @@
+#ifndef ___types___hpp___
+#define ___types___hpp___
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <malloc.h>
+#include <stdint.h>
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+#include <cassert>
+#endif
