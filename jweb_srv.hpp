501508e4acf07a6ab5c4e0b1e429fe9245cf1646
DimitrisVlachos <DimitrisV22@gmail.com>
2014-06-22 18:24:01 -0400

    Update jweb_srv.hpp

--- jweb_srv.hpp
+++ jweb_srv.hpp
@@ -1,10 +1,12 @@
 
 /*
+
 	Description : Simple multithreaded web server app in C++ (for educational porpuses only)
 	Author		: Dimitris Vlachos (DimitrisV22@gmail.com) (http://github.com/DimitrisVlachos)
 	Date		: 22/6/2014
 	Instructions:
 				-Compile with : g++ mt.cpp server_demo.cpp -O2 -lpthread -o server
+				-Create a directory that matches work_dir argument passed in web_server_c::init() function and place your files
 				-Run : ./server (Due to port 80 allocation you (most-likely) have to run the server as root)
 */
 
