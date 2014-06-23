c5ec6440ba5da265c7d1ea71815802791ca9e3c9
DimitrisVlachos <DimitrisV22@gmail.com>
2014-06-23 13:46:28 -0400

    Update demo.cpp

--- demo.cpp
+++ demo.cpp
@@ -13,14 +13,21 @@
 
 int main() { 
 	const std::string work_dir = "root";
-	const int32_t default_port = 8080;
+	const std::string php_cgi_path = "/usr/bin/php-cgi/";	
+	const std::string php_cgi_binary = "php-cgi";
+	const int32_t default_port = 80;
 	const uint32_t max_threads = 4;
 	web_server_c www;
 
 	if (!www.init(work_dir,default_port,max_threads)) {
 		std::cout << "Init failed : " << www.get_error_string() << std::endl;
 		return -1;
-	} 
+	}
+
+	//optional
+	if (!www.init_php(php_cgi_path,php_cgi_binary)) {
+		std::cout << "Warning : Init PHP CGI SCRIPT failed : " << www.get_error_string() << std::endl;
+	}
 
 	do {	
 	} while (www.run());
